e39813aec41f27b76e1186c9c4bdb37d16cde265
Peter Stoyanov <desireon98@gmail.com>
2025-08-10 18:09:46 +0300

    feat: add service entity

---
+++ migrations/Version20250810150054.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250810150054 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create service table with unique slug';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_E19D9AD2989D9B62 ON service (slug)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE service');
+    }
+}
