728c2091488d6036e4274e3fd512317692cdead0
Peter Stoyanov <desireon98@gmail.com>
2025-08-11 15:53:08 +0300

    feat: list groomers by city and service

--- src/Repository/GroomerProfileRepository.php
+++ src/Repository/GroomerProfileRepository.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\City;
 use App\Entity\GroomerProfile;
+use App\Entity\Service;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -31,5 +33,26 @@ class GroomerProfileRepository extends ServiceEntityRepository
             ->setParameter('slug', $slug)
             ->getQuery()
             ->getOneOrNullResult();
+    }
+
+    /**
+     * @return array<int, GroomerProfile>
+     */
+    public function findByCityAndService(City $city, Service $service, int $limit = 20, int $offset = 0): array
+    {
+        $query = $this->createQueryBuilder('g')
+            ->innerJoin('g.services', 's')
+            ->where('g.city = :city')
+            ->andWhere('s = :service')
+            ->setParameter('city', $city)
+            ->setParameter('service', $service)
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->getQuery();
+
+        /** @var array<int, GroomerProfile> $result */
+        $result = $query->getResult();
+
+        return $result;
     }
 }
