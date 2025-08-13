a533e5ab89e710a52c944a952e542b5c4c7bb4ac
Peter Stoyanov <desireon98@gmail.com>
2025-08-13 23:14:25 +0300

    refactor: replace Stimulus autocomplete with vanilla JS

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -8,3 +7,4 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
