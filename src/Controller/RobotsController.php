6c2b75fef5b7a0c74533eab69d3eba31428d9293
Peter Stoyanov <desireon98@gmail.com>
2025-08-16 14:57:56 +0300

    feat: add robots.txt and preview noindex header

---
+++ src/Controller/RobotsController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class RobotsController extends AbstractController
+{
+    #[Route('/robots.txt', name: 'app_robots', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        $baseDir = $this->getParameter('kernel.project_dir');
+        $baseDir = \is_string($baseDir) ? $baseDir : \dirname(__DIR__, 2);
+        $path = $baseDir.'/public/robots.txt';
+
+        $content = @file_get_contents($path);
+        $content = \is_string($content) ? $content : '';
+
+        return new Response($content, Response::HTTP_OK, ['Content-Type' => 'text/plain']);
+    }
+}
---
+++ tests/Functional/RobotsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class RobotsTest extends WebTestCase
+{
+    public function testRobotsTxtServed(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/robots.txt');
+        self::assertResponseIsSuccessful();
+        $content = (string) $client->getResponse()->getContent();
+        self::assertStringContainsString('Disallow: /admin', $content);
+        self::assertStringContainsString('Disallow: /blog/tag/*?page=', $content);
+    }
+
+    public function testPreviewRouteGetsNoIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/_preview/test');
+        self::assertResponseIsSuccessful();
+        self::assertSame('noindex', $client->getResponse()->headers->get('X-Robots-Tag'));
+    }
+}
