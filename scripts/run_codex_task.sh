df91f5042c4ee75a76ae25863fd257dc0cab9055
Petar Stoqnov <desireon98@gmail.com>
2025-08-17 22:51:12 +0300

    wip

--- scripts/run_codex_task.sh
+++ scripts/run_codex_task.sh
@@ -27,7 +27,7 @@ else
   git checkout -b "$BRANCH" "origin/$BASE_BRANCH"
 fi
 
-git rm --cached .task_status.json
+rm -rf .task_status.json
 
 # Rebase on top of latest base branch (autostash to handle local changes)
 git rebase --autostash "origin/$BASE_BRANCH"
