1362a9c857c557bbe8c7492681b79e948d830f29
Petar Stoqnov <desireon98@gmail.com>
2025-08-25 17:58:21 +0300

    update city-cards

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,7 @@ import './styles/blocks/carousel-mmGYQrl.css';
 import './styles/blocks/groomer-card.css';
 import './styles/components/badge.css';
 import './styles/components/mobile-cta.css';
+import './styles/components/city-cards.css';
 import './js/mobile-cta.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
