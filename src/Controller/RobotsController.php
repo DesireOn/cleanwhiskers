722e71a7e27df034713f568ed48ef4147cb51cda
Petar Stoqnov <desireon98@gmail.com>
2025-08-28 15:39:13 +0300

    update robots

--- src/Controller/RobotsController.php
+++ src/Controller/RobotsController.php
@@ -17,7 +17,17 @@ final class RobotsController extends AbstractController
         $host = $request->getHost();
         $productionHosts = ['cleanwhiskers.com', 'www.cleanwhiskers.com'];
 
-        $content = "User-agent: *\nDisallow: /\n";
+        if (!\in_array($host, $productionHosts, true)) {
+            $content = "User-agent: *\nDisallow: /\n";
+        } else {
+            $baseDir = $this->getParameter('kernel.project_dir');
+            $baseDir = \is_string($baseDir) ? $baseDir : \dirname(__DIR__, 2);
+            $path = $baseDir.'/public/robots.txt';
+
+            $content = @file_get_contents($path);
+            $content = \is_string($content) ? $content : '';
+            $content .= "\nSitemap: ".$request->getScheme().'://'.$host.'/sitemap.xml';
+        }
 
         return new Response($content, Response::HTTP_OK, ['Content-Type' => 'text/plain']);
     }
